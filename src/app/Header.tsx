355b1284ad7ccd9d572d0e1ebd7c17b375d036cf
chungdeptraivl <thanhchungrc229@gmail.com>
2024-04-15 11:53:11 +0700

    up code

---
+++ src/app/Header.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <Button className="p-9 font-bold" variant={"ghost"}>
+        <Link href={"/"}>Yeu em</Link>
+      </Button>
+
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
